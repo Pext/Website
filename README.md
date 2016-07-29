9a7dcc4a3723ba5a60a0e8f31142a680c104f099
Sylvia van Os <iamsylvie@openmailbox.org>
2016-07-29 02:45:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pext Website
+A simple website for [Pext](https://github.com/Pext/Pext).
+
+# License
+CC0 1.0
---
+++ main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $("#pext-purpose").typed({
+        strings: ["password manager", "todo-list manager", "emoji selector"],
+        typeSpeed: 0,
+	backDelay: 2000,
+	shuffle: true,
+	loop: true
+    });
+});
