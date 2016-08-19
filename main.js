d5b695fdf7198e78b01c3a85aad20f6f42ebb16e
Sylvia van Os <iamsylvie@openmailbox.org>
2016-08-19 17:13:53 +0200

    Add reference to ssh module

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(function(){
     $("#pext-purpose").typed({
-        strings: ["password manager", "todo-list manager", "emoji selector"],
+        strings: ["password manager", "todo-list manager", "emoji selector", "ssh server list"],
         typeSpeed: 0,
         backDelay: 2000,
         shuffle: true,
