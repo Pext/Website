dcda15738ee54a5a66c72f811b1b38f780db3168
Sylvia van Os <iamsylvie@openmailbox.org>
2016-08-26 18:20:38 +0200

    Get rid of typed.js

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 $(function(){
-    $("#pext-purpose").typed({
-        strings: ["password manager", "todo-list manager", "emoji selector", "ssh server list", "weather app"],
-        typeSpeed: 0,
-        backDelay: 2000,
-        shuffle: true,
-        loop: true
-    });
+    var changeString = function() {
+        var strings = ["password manager", "todo-list manager", "emoji selector", "ssh server list", "weather app"];
+
+        $("#pext-purpose").fadeOut(function() {
+            currentString = $("#pext-purpose").text();
+
+            do {
+                newString = strings[Math.floor(Math.random() * strings.length)];
+            } while (newString == currentString);
+
+            $("#pext-purpose").text(newString);
+            $("#pext-purpose").fadeIn();
+        })
+    };
+
+    window.setInterval(changeString, 3000);
+    changeString();
 });
