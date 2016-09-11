0df52519749418f3b5e37e80891300fc3d37afcc
Sylvia van Os <iamsylvie@openmailbox.org>
2016-09-11 15:16:59 +0200

    Rewrite in Bootstrap, add Pext logo, promote NotABug more

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Pext Website
 A simple website for [Pext](https://github.com/Pext/Pext).
 
+To make changes, edit source and run generate.sh.
+
 # License
-CC0 1.0
+CC0 1.0 (with the exception of the Pext logo and Bootstrap, which are used
+under their original licenses)
---
+++ generate.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Generate GitHub version
+cp source index.html
+sed -i 's/{{ prefer }}/If you prefer the buttons to link to NotABug instead of GitHub, <a href="notabug.html">click here<\/a>./' index.html
+sed -i 's/{{ core_link }}/https:\/\/github.com\/Pext\/Pext/' index.html
+sed -i 's/{{ emoji_link }}/https:\/\/github.com\/Pext\/pext_module_emoji/' index.html
+sed -i 's/{{ openweathermap_link }}/https:\/\/github.com\/Pext\/pext_module_openweathermap/' index.html
+sed -i 's/{{ pass_link }}/https:\/\/github.com\/Pext\/pext_module_pass/' index.html
+sed -i 's/{{ radiobrowser_link }}/https:\/\/github.com\/Pext\/pext_module_radiobrowser/' index.html
+sed -i 's/{{ ssh_link }}/https:\/\/github.com\/Pext\/pext_module_ssh/' index.html
+sed -i 's/{{ todo_sh_link }}/https:\/\/github.com\/Pext\/pext_module_todo.sh/' index.html
+
+# Generate NotABug version
+cp source notabug.html
+sed -i 's/{{ prefer }}/If you prefer the buttons to link to GitHub instead of NotABug, <a href="index.html">click here<\/a>./' notabug.html
+sed -i 's/{{ core_link }}/https:\/\/notabug.org\/SylvieLorxu\/Pext/' notabug.html
+sed -i 's/{{ emoji_link }}/https:\/\/notabug.org\/Pext\/pext_module_emoji/' notabug.html
+sed -i 's/{{ openweathermap_link }}/https:\/\/notabug.org\/Pext\/pext_module_openweathermap/' notabug.html
+sed -i 's/{{ pass_link }}/https:\/\/notabug.org\/Pext\/pext_module_pass/' notabug.html
+sed -i 's/{{ radiobrowser_link }}/https:\/\/notabug.org\/Pext\/pext_module_radiobrowser/' notabug.html
+sed -i 's/{{ ssh_link }}/https:\/\/notabug.org\/Pext\/pext_module_ssh/' notabug.html
+sed -i 's/{{ todo_sh_link }}/https:\/\/notabug.org\/Pext\/pext_module_todo.sh/' notabug.html
