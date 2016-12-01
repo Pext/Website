4fa7bb8e54c424698313c53fcdfa798cbbe186f5
Sylvia van Os <iamsylvie@openmailbox.org>
2016-12-01 21:29:21 +0100

    Add launcher module

--- generate.sh
+++ generate.sh
@@ -5,6 +5,7 @@ cp source index.html
 sed -i 's/{{ prefer }}/If you prefer the buttons to link to NotABug instead of GitHub, <a href="notabug.html">click here<\/a>./' index.html
 sed -i 's/{{ core_link }}/https:\/\/github.com\/Pext\/Pext/' index.html
 sed -i 's/{{ emoji_link }}/https:\/\/github.com\/Pext\/pext_module_emoji/' index.html
+sed -i 's/{{ launcher_link }}/https:\/\/github.com\/Pext\/pext_module_launcher/' index.html
 sed -i 's/{{ openweathermap_link }}/https:\/\/github.com\/Pext\/pext_module_openweathermap/' index.html
 sed -i 's/{{ pass_link }}/https:\/\/github.com\/Pext\/pext_module_pass/' index.html
 sed -i 's/{{ radiobrowser_link }}/https:\/\/github.com\/Pext\/pext_module_radiobrowser/' index.html
@@ -18,6 +19,7 @@ cp source notabug.html
 sed -i 's/{{ prefer }}/If you prefer the buttons to link to GitHub instead of NotABug, <a href="index.html">click here<\/a>./' notabug.html
 sed -i 's/{{ core_link }}/https:\/\/notabug.org\/SylvieLorxu\/Pext/' notabug.html
 sed -i 's/{{ emoji_link }}/https:\/\/notabug.org\/Pext\/pext_module_emoji/' notabug.html
+sed -i 's/{{ launcher_link }}/https:\/\/notabug.org\/Pext\/pext_module_launcher/' notabug.html
 sed -i 's/{{ openweathermap_link }}/https:\/\/notabug.org\/Pext\/pext_module_openweathermap/' notabug.html
 sed -i 's/{{ pass_link }}/https:\/\/notabug.org\/Pext\/pext_module_pass/' notabug.html
 sed -i 's/{{ radiobrowser_link }}/https:\/\/notabug.org\/Pext\/pext_module_radiobrowser/' notabug.html
